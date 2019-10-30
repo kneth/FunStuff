143edd06a1cda3b25baf72a2daa1411122ebde0d
Kenneth Geisshirt <geisshirt@gmail.com>
2019-10-30 12:49:59 +0100

    One more fraction in Go

--- Go/fraction.go
+++ Go/fraction.go
@@ -39,7 +39,11 @@ func (f1 Fraction) add(f2 Fraction) Fraction {
 func main() {
 	f1 := Fraction{numerator: 1, denominator: 3}
 	f2 := Fraction{numerator: 2, denominator: 5}
-	var f3 = f1.add(f2)
+	f3 := Fraction{numerator: 2, denominator: 4}
 
-	fmt.Printf("%d / %d\n", f3.numerator, f3.denominator)
+	var f4 = f1.add(f2)
+	var f5 = f1.add(f3)
+
+	fmt.Printf("%d / %d\n", f4.numerator, f4.denominator)
+	fmt.Printf("%d / %d\n", f5.numerator, f5.denominator)
 }
