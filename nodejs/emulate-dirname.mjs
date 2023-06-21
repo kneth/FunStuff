843f5ec87efa213fa2a26bde3988890cdb8cad38
Kenneth Geisshirt <k@zigzak.net>
2023-06-21 13:23:49 +0200

    [node.js] play with dirname

---
+++ nodejs/emulate-dirname.mjs
@@ -0,0 +1,8 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(`__filename = ${__filename}`);
+console.log(`__dirname = ${__dirname}`);
