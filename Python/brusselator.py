5b1909208ed8f89b487349802c12fcbfd07bab03
Kenneth Geisshirt <k@zigzak.net>
2024-10-24 17:38:41 +0200

    Formatting

--- Python/brusselator.py
+++ Python/brusselator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # Simulator of Brusselator
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
 
 A = 1
 B = 3
+
 
 def brussel(t, c):
     X = c[0]
@@ -15,6 +16,7 @@ def brussel(t, c):
     dX = A + X*X*Y - B*X - X
     dY = B*X - X*X*Y
     return [dX, dY]
+
 
 sol = solve_ivp(brussel, [0, 30], [1.0, 1.0], dense_output=True)
 
